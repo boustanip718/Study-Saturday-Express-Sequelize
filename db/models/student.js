10c8e7a1e85204f591e6ce36aa4bf22d1fc87acd
Peter Boustani <boustanip718@gmail.com>
2020-10-03 11:16:28 -0400

    cycle-1 complete :)

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,27 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('students', {
+   firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+   },
+   lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+   },
+   email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+         isEmail: true
+      }
+   }
+});
+
+Student.beforeCreate(async (student) => {
+   student.firstName = await student.firstName[0].toUpperCase() + student.firstName.slice(1);
+   student.lastName = await student.lastName[0].toUpperCase() + student.lastName.slice(1);
+});
 
 module.exports = Student;
